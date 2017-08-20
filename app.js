9148c6c12eb23018ef22589804912523f5a49d0b
vector <295253797@qq.com>
2017-08-20 22:31:26 +0800

    change new project

--- app.js
+++ app.js
@@ -1,3 +1,5 @@
+
+
 var express = require('express');
 var path = require('path');
 var favicon = require('serve-favicon');
@@ -5,16 +7,19 @@ var logger = require('morgan');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
 
-var ejs = require('ejs');
-var routes = require('./routes/index');
-var users = require('./routes/users');
-
+var routes = require('./controllers/index');
+var users = require('./controllers/users');
+var admin = require('./controllers/admin');
+var tag = require('./controllers/tag');
+var article = require('./controllers/article');
 var app = express();
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
-app.engine('html',ejs.__express)
-app.set('view engine','html'); 
+//设置模板的后缀是html
+app.engine('html', require('ejs').renderFile);
+//指定总模板
+app.set('view engine', 'html');
 
 // uncomment after placing your favicon in /public
 //app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
@@ -26,7 +31,9 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', routes);
 app.use('/users', users);
-
+app.use('/admin', admin);
+app.use('/tag', tag);
+app.use('/article', article);
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
   var err = new Error('Not Found');
